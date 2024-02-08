83400682435e651e35a6f586b9d1de93d8f3b962
Rukiye Aydın <rukiyeaydinlive@gmail.com>
2024-02-08 15:24:27 +0300

    navbar design changed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,26 +5,23 @@ import {
   TEDropdownToggle,
   TEDropdownMenu,
   TEDropdownItem,
-  TERipple,
 } from "tw-elements-react";
 
 const Navbar = () => {
   return (
-    <div className='bg-zinc-900 flex items-center justify-between p-5'>
-        <Link to='/' className='text-white text-2xl font-bold'>DN</Link>
-        <div className='flex items-center justify-center'>
-          <Link className='text-white bg-emerald-500 hover:bg-emerald-400 font-bold px-3 py-2 rounded-md transition-all mr-3'>New</Link>
-          <TEDropdown className="flex justify-center">
-            <TERipple rippleColor="light">
-              <TEDropdownToggle className="flex items-center text-white text-xl py-1 px-2 rounded-md border-2 border-purple-400 hover:bg-purple-400 transition-all"><p>R</p></TEDropdownToggle>
-            </TERipple>
-            <TEDropdownMenu className='bg-zinc-900 text-white border-2 border-purple-400 mt-1 p-3 rounded-md'>
-              <TEDropdownItem className='mb-2'><Link to='/my-notes' className='hover:text-purple-300'>My Notes</Link></TEDropdownItem>
-              <TEDropdownItem className='mb-2'><Link to="/profile" className='hover:text-purple-300'>Profile</Link></TEDropdownItem>
-              <TEDropdownItem><Link className='hover:text-purple-300'>Log Out</Link></TEDropdownItem>
-            </TEDropdownMenu>
-          </TEDropdown>
-        </div>
+    <div className='flex items-center justify-end w-full'>
+      <div className='w-24 h-24 flex items-center justify-center bg-black rounded-bl-full pb-5 pl-5'>
+        <TEDropdown className="flex justify-center">
+          <TEDropdownToggle className="flex items-center text-white text-2xl py-1 px-2 rounded-md border-2 border-emerald-500 hover:bg-emerald-500 transition-all"><p>R</p></TEDropdownToggle>
+          <TEDropdownMenu position='dropleft' className='bg-black border-2 border-emerald-500 text-white p-3 mr-1 rounded-md text-right'>
+            <TEDropdownItem className='mb-2'><Link to='/' className='hover:text-emerald-400'>Home</Link></TEDropdownItem>
+            <TEDropdownItem className='mb-2'><Link to='/my-notes' className='hover:text-emerald-400'>My Notes</Link></TEDropdownItem>
+            <TEDropdownItem className='mb-2'><Link to="/profile" className='hover:text-emerald-400'>Profile</Link></TEDropdownItem>
+            <hr className="my-2 h-0 border border-t-0 border-solid border-white opacity-75 dark:border-white" />
+            <TEDropdownItem><Link className='hover:text-red-500'>Log Out</Link></TEDropdownItem>
+          </TEDropdownMenu>
+        </TEDropdown>
+      </div>
     </div>
   )
 }
